0f991f6c1576ba7ac67515a24b12254e402a9fee
VictorOmondi1997 <info.victoromondi@gmail.com>
2019-05-29 10:55:15 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Tilt Js
+$(document).ready(function () {
+    "use strict";
+
+    var window_width = $(window).width(),
+        window_height = window.innerHeight,
+        header_height = $(".default-header").height(),
+        header_height_static = $(".site-header.static").outerHeight(),
+        fitscreen = window_height - header_height;
+
+    $('.single-price').tilt({
+        maxTilt: 9,
+    });
+});
